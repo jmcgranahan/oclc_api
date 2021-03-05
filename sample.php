c423d21471980db6c7ff4f2a6b35fedb24ef9e5d
Jamen McGranahan <jthejetmcg@gmail.com>
2021-03-05 14:53:23 -0600

    oclc script update

--- sample.php
+++ sample.php
@@ -7,14 +7,16 @@
    
    include("containers.inc.php");
 
-   /* Assign the first argument passed to the service variable - sets whether Prod or Sand */
+   /* Assign the first argument passed to the service variable - sets whether Prod or Sand - and look for filename */
    if ($argc > 1) {
      $service=$argv[1];
+     $filename=$argv[2];
    } else {
-     print "Please supply a valid argument to the command line - either Production or Sandbox.";
+     print "Please supply a valid argument to the command line - either Production or Sandbox - and enter the name of the file to process.";
      die();
    }
 
+   /* assign keys based upon user input */
    if ($service == "Sandbox") {
      $key = $oclc_keys["sandbox"]["key"];
      $secret = $oclc_keys["sandbox"]["secret"];
@@ -31,24 +33,35 @@
     print "Please enter either Sandbox or Production from the command line";
    }
 
+   /* open file that contains the OCLC numbers to review */
+   $file=fopen($filename,'r');
+
    $wskey = new WSKey($key, $secret);
    
-   $url = 'https://worldcat.org/bib/checkcontrolnumbers?oclcNumbers=31777094';
+   $baseurl = 'https://worldcat.org/bib/checkcontrolnumbers?oclcNumbers=';
    
    $user = new User($registry_id, $principal_id, $idns);
    $options = array('user'=> $user);
    
-   $authorizationHeader = $wskey->getHMACSignature('GET', $url, $options);
+   while(!feof($file)) {
+     $oclcnum=gets($file);
+     $url=$baseurl.$oclcnum;
+     process($url,$options);
+   }
+
+   function process($url,$options) {
+     $authorizationHeader = $wskey->getHMACSignature('GET', $url, $options);
     
-   $client = new Client();
-   $headers = array();
-   $headers['Authorization'] = $authorizationHeader;
+     $client = new Client();
+     $headers = array();
+     $headers['Authorization'] = $authorizationHeader;
    
-   try {
+     try {
         $response = $client->request('GET', $url, ['headers' => $headers]);
         echo $response->getBody(TRUE);
-   } catch (RequestException $error) {
+     } catch (RequestException $error) {
         echo $error->getResponse()->getStatusCode();
         echo $error->getResponse()->getBody(true);
+     }
    }
 ?>
